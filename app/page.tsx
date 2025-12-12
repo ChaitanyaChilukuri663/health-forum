cd206ee973770af4d3106dda0b9b1bb4f8a3b6fb
ChaitanyaChilukuri663 <chaitanya.chiilukuri@gmail.com>
2025-12-12 11:49:55 +0530

    Updated page to dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { supabase } from "@/lib/supabaseClient";
 import Link from "next/link";
-
+export const dynamic = "force-dynamic";
 export default async function Home() {
 
   // Fetch posts from Supabase
